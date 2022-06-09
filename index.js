f58e756ea4cd13e31cbff056982f2807e3950214
Mayce <mayce@Meenas-MacBook-Pro.local>
2022-06-09 09:06:50 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  function isPalindrome(str) {
+    const strArr = str.split("")
+    const strReversedArr = str.split("").reverse()
+    let a = 0;
+    for (let i in strReversedArr) {
+      if (strReversedArr[i] === strArr[i]) {
+        a++
+      }
+    }
+    if (a === strArr.length) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
 }
 
 /* 
